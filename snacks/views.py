3113adc0b51186fa3562a53adeeea2c6bdcc3fa4
islam alghoul <islamalghoul1997>
2022-12-03 11:40:06 +0200

    snack

--- snacks/views.py
+++ snacks/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView
+from django.views.generic import ListView,DetailView
 from .models import Snack
 
-class Snacks(TemplateView):
+class Snacks(ListView):
     template_name='Home.html'
+    model= Snack
 
-class SnackList(ListView):
+class SnackList(DetailView):
     template_name='snack_detail.html'
     model= Snack
